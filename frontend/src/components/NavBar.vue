abcab3688f85f5f9cf14b2959e3355cc17ff04ff
mrodrig <rodrigues.mi@northeastern.edu>
2024-08-21 22:47:41 -0400

    fix: resolve 429 errors on google profile photos
    Fix came from this StackOverflow post: https://stackoverflow.com/questions/56242788/http-403-on-images-loaded-from-googleusercontent-com

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -82,7 +82,7 @@ export default {
               <MenuButton class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                 <span class="absolute -inset-1.5" />
                 <span class="sr-only">Open user menu</span>
-                <img class="h-8 w-8 rounded-full" :src="userIconUrl" alt="Profile Image" />
+                <img class="h-8 w-8 rounded-full" :src="userIconUrl" alt="Profile Image" referrerpolicy="no-referrer" />
               </MenuButton>
             </div>
             <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
